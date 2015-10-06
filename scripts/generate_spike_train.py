db1c3a76271c113cbd733465f2798d4ac604f6cb
Andris Ecker <ecker.andris@gmail.com>
2015-10-06 09:53:19 +0200

    spike train generation works in python

--- scripts/generate_spike_train.py
+++ scripts/generate_spike_train.py
@@ -3,31 +3,39 @@
 
 import numpy as np
 import os
+import random
 from poisson_proc import inhomPoisson
+
+fOut = 'spikeTrainsR.npz'
 
 SWBasePath = os.path.split(os.path.split(__file__)[0])[0]
 
+nPop = 4000  # of populations
+nNeuron = 1  # of neurons in one population
 
-theta = 7.0  # theta frequence [Hz]
-avgRateInField = 20.0  # avg. in-field firing rate [ms]
-vMice = 32.43567842  # velocity of the mice [cm/s]
-lPlaceField = 30.0  # length of the place field [cm]
-lRoute = 300.0  # circumference [cm]
-
-nPop = 50  # # of populations
-nNeuron = 80 # # of neurons in one population
-
+seed = 0
 
 spikeTrains = []
 
-for pop in range(0, nPop):
-    for neuron in range(0, nNeuron):
-        tmp = inhomPoisson(0, pop, theta, avgRateInField, vMice, lPlaceField,lRoute)
-        spikeTrains.append(tmp)
-        print pop + 1, '/', neuron + 1, 'done from', nPop, '/', nNeuron
+if nNeuron != 1:
 
+    for pop in range(0, nPop):
+        for neuron in range(0, nNeuron):
+            spikeTrains.append(inhomPoisson(float(nPop), 0.0, float(pop), seed))
+            print str(pop + 1), '/', str(neuron + 1), 'done from', str(nPop), '/', str(nNeuron)
+            seed += 1
+else:
+
+    neurons = np.linspace(0, nPop - 1, nPop)
+    random.shuffle(neurons)
+    for i, neuron in enumerate(neurons):
+        spikeTrains.append(inhomPoisson(float(nPop), 0.0, float(neuron), seed))
+        print str(i + 1), 'done from', str(nPop)
+        seed += 1
 
 assert len(spikeTrains) == nPop * nNeuron
 
-fOut = os.path.join(SWBasePath, 'files', 'spikeTrains.npz')
-np.savez(fOut, spikeTrains=spikeTrains)
+
+# save results to .npz
+fName = os.path.join(SWBasePath, 'files', fOut)
+np.savez(fName, spikeTrains=spikeTrains)
--- scripts/poisson_proc.py
+++ scripts/poisson_proc.py
@@ -3,77 +3,92 @@
 
 import numpy as np
 
-def generateFiringRate(t, pop, phase0, theta, avgRateInField, vMice, lPlaceField,lRoute):
+def generateFiringRate(nPop, pop, phase0, t):
+    '''
+    Calculates the lambda parameter of the Poisson process, that represent the firing rate in CA3 (pyr. cells)
+    (takes preferred place and phase precession into account)
+    :param nPop: # of neuron populations
+    :param t: time
+    :param pop: current population
+    :param phase0: initial phase (used for modeling phase precession)
+    :return: lambda: calculated lambda parameter of the Poisson process
+    '''
+
+    theta = 7.0  # theta frequence [Hz]
+    avgRateInField = 20.0  # avg. in-field firing rate [ms]
+    vMice = 32.43567842  # velocity of the mice [cm/s]
+    lPlaceField = 30.0  # length of the place field [cm]
+    lRoute = 300.0  # circumference [cm]
 
     r = lRoute / (2 * np.pi)  #radius [cm]
     tRoute = lRoute / vMice  # [s]
-    wMice=2 * np.pi / tRoute  # angular velocity
+
+    wMice = 2 * np.pi / tRoute  # angular velocity
+    x = np.mod(wMice * t, 2 * np.pi)  # position of the mice [rad]
 
     phiPFRad = lPlaceField / r  # angle of place field [rad]
-    # phiPF = 180 * (phiPFRad / np.pi)  # angle of place field [deg]
 
-    nPop = 50
+    phiStart = (pop / nPop) * 2 * np.pi
+    phiEnd = phiStart + phiPFRad
 
-    phiStart = pop * 2 * np.pi * 1/nPop
-    phiEnd = np.mod(phiStart + phiPFRad, 2 * np.pi)  # ???
-
-    x = np.mod(wMice * t, 2 * np.pi)  # position of the mice [rad]
-    y = phase0 + 2 * np.pi * theta * t  # ??? 't=0 -nál 0 foktól indul'
-
-    shift = phiStart + phiPFRad / 2
+    assert phiStart < phiEnd
 
     # phase precession
-    m = - (x - phiStart) * 2 * np.pi / phiPFRad  # prefered phase: f(actuall position within the place field)
+    y = phase0 + 2 * np.pi * theta * t
+    shift = phiStart + phiPFRad / 2
+    m = - (x - phiStart) * 2 * np.pi / phiPFRad  # prefered phase: f(current position within the place field)
 
-    sig = 0.5  # deviation of phase-locking (von Misses distribution)
+    sig = 0.5  # deviation of phase-locking (von Misses distribution -> see lambda2)
     s = 1.0 / sig
 
-    if phiStart < phiEnd:
-        if phiStart <= x and x < phiEnd:
+    if phiStart <= x and x < phiEnd:  # if the mice is in the place field (of the current population)
 
-            lambda1 = np.cos((2 * np.pi) / (2 * phiPFRad) * (x - shift)) * avgRateInField
-            lambda2 = np.exp(s * np.cos(y - m)) / np.exp(s)
-
-        else:
-
-            lambda1 = 0
-            lambda2 = 1
+        lambda1 = np.cos((2 * np.pi) / (2 * phiPFRad) * (x - shift)) * avgRateInField
+        lambda2 = np.exp(s * np.cos(y - m)) / np.exp(s)
 
     else:
-        if phiStart <= x or x < phiEnd:
 
-            lambda1 = np.cos((2 * np.pi) / (2 * phiPFRad) * (x - shift)) * avgRateInField
-            lambda2 = np.exp(s * np.cos(y - m)) / np.exp(s)
-
-        else:
-
-            lambda1 = 0
-            lambda2 = 1
+        lambda1 = 0
+        lambda2 = 1
 
     lambdaP = lambda1 * lambda2
 
     return lambdaP
 
 
-def inhomPoisson(phase0, pop, theta, avgRateInField, vMice, lPlaceField,lRoute):
+def inhomPoisson(nPop, phase0, pop, seed):
+    '''
+    Makes a homogenous Poisson process and transfer it to inhomogenous via deleting spikes
+    (based on an other Poisson process made by generateFiringRates)
+    :param nPop: # of neuron populations (see generateFiringRate)
+    :param phase0: initial phase (see generateFiringRate)
+    :param pop: current population (see generateFiringRate)
+    :param seed: seed for random number generation
+    :return: inhP: python list which represent an inhomogenos Poisson process
+    '''
 
     lambdaE = 20.0
+    mu = 1.0 / lambdaE
     tMax = 500.0  # [ms]
 
     homP = []  # homogeneous Poisson process
-    homP.append(np.random.exponential(1.0 / lambdaE))
+    np.random.seed(seed)
+    exprnd = -mu * np.log(np.random.rand(1))[0]  # MATLAB's random exponential number
+    homP.append(exprnd)
     i = 0
     while homP[i] < tMax:
-        tmp = homP[-1] + np.random.exponential(1.0 / lambdaE)
+        np.random.seed(seed + i + 1)
+        exprnd = -mu * np.log(np.random.rand(1))[0]  # # MATLAB's random exponential number
+        tmp = homP[-1] + exprnd
         homP.append(tmp)
         i += 1
 
-    del homP[-1]
+    del homP[-1]  # delete the last element which is higher than tMax
 
     inhP = []  # inhomogeneous Poisson process
-    for spike in homP:
-        tmp = generateFiringRate(spike, pop, phase0, theta, avgRateInField, vMice, lPlaceField,lRoute)
-        if  tmp / lambdaE >= np.random.rand(1):
-            inhP.append(spike)
+    for i, t in enumerate(homP):
+        np.random.seed(seed + i + 1)
+        if  generateFiringRate(nPop, pop, phase0, t) / lambdaE >= np.random.rand(1):
+            inhP.append(t)
 
     return inhP
