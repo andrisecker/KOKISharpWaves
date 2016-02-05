3aef85bcc50ab15c4aa6f5c7714cb34e8ff4f4fe
Andris Ecker <ecker.andris@gmail.com>
2016-02-05 23:04:37 +0100

    works! (taking only cells with aligning PFs into account)

--- scripts/bayesian_decoding.py
+++ scripts/bayesian_decoding.py
@@ -12,9 +12,8 @@ import os
 
 fInSpikes = 'spikes.npz'
 fInPF = 'PFstarts.npz'
-fOut = 'route.npz'
-
-tempRes = 0.05  # [s]
+fOut = 'route_0.01.npz'
+tempRes = 0.01  # [s]
 spaRes = 2*np.pi / 360.0  # [rad] ( == 1 degree)
 N = 4000
 
@@ -31,13 +30,25 @@ phiPFRad = lPlaceField / r  # (angle of) place field [rad]
 avgRateInField = 20.0  # avg. in-field firing rate [Hz]
 
 
-# calculate firing rates (\tau_i(x))  !!! calculate not estimate
+# list of overlapping place fields
 fName = os.path.join(SWBasePath, 'files', fInPF)
 npzFile = np.load(fName)
-pfStarts = npzFile['pfStarts']
+pfStarts = npzFile['pfStarts'].tolist()
 
+overlappingPFs = []
+for pfStart in pfStarts:
+    overlap = []
+    pfEnd = np.mod(pfStart + phiPFRad, 2*np.pi)
+    if pfStart < (2*np.pi - phiPFRad):
+        overlap = [i for i, val in enumerate(pfStarts) if pfStart <= val and val < pfEnd]
+    else:
+        overlap = [i for i, val in enumerate(pfStarts) if pfStart <= val or val < pfEnd]
+
+    overlappingPFs.append(overlap)
+
+
+# calculate firing rates (\tau_i(x))  !!! calculate not estimate
 rates = []
-
 for i in range(0, N):
     tau = np.zeros((1, int(2*np.pi / spaRes)))
 
@@ -65,17 +76,38 @@ spiketimes = npzFile['spiketimes']
 
 
 # log(likelihood): log(Pr(spikes|x)) = \sum_{i=1}^N n_ilog(\frac{\Delta t \tau_i(x)}{n_i!}) - \Delta t \sum_{i=1}^N \tau_i(x)
-delta_t = tempRes  # in s
 
+# taking only the cells into account, whose have overlapping place fields with a cell, that fired in the bin
+cellROI = []
+binSpikes = []
+
+for t1, t2 in zip(samplingTimes[:-1], samplingTimes[1:]):
+    count = 0
+    tmp = []  # will be a list of list (cells that have to be taken into account)
+    for i in range(0, N):
+        n_i = ((t1 < spiketimes[i]) & (spiketimes[i] < t2)).sum()  # #{spikes of the i-th cell in the bin}
+        if n_i != 0:
+            tmp.append(overlappingPFs[i])
+            count += n_i
+    tmp2 = list(set(sorted([item for sublist in tmp for item in sublist])))
+    cellROI.append(tmp2)
+    binSpikes.append(count)
+
+print 'average spikes/bin:', np.mean(binSpikes)
+
+
+delta_t = tempRes  # in s
 route = []
 ML = []
+
+bin = 0
 for t1, t2 in zip(samplingTimes[:-1], samplingTimes[1:]):
     likelihoods = []
     for indPhi in range(0, len(spatialPoints)):
         likelihood1 = 0
         likelihood2 = 0
 
-        for i in range(0, N):
+        for i in cellROI[bin]:  # instead of "for i in range(0, N):"
             tmp = 0
 
             n_i = ((t1 < spiketimes[i]) & (spiketimes[i] < t2)).sum()  # #{spikes of the i-th cell in the bin}
@@ -86,17 +118,22 @@ for t1, t2 in zip(samplingTimes[:-1], samplingTimes[1:]):
 
             likelihood1 += tmp
             likelihood2 += tau_i_phi
-
         likelihood = likelihood1 - delta_t * likelihood2
+
         likelihoods.append(likelihood)
+        likelihoods = [np.nan if x == 0 else x for x in likelihoods]  # change 0s to np.nan
+        if np.isnan(likelihoods).all():
+            likelihoods[0] = 0
 
     # search for the maximum of the likelihoods in a given sampling time
-    id = np.argmax(likelihoods)
+    id = np.nanargmax(likelihoods)
     maxLikelihood = likelihoods[id]
     place = spatialPoints[id]
     route.append(place)
     ML.append(maxLikelihood)
     print 'sampling time:', str(t2 * 1000), '[ms]:', str(place), '[rad] ML:', maxLikelihood
+    bin += 1
+
 
 fName = os.path.join(SWBasePath, 'files', fOut)
 np.savez(fName, route=route, ML=ML)
@@ -113,11 +150,21 @@ for i in spikes:
 fig = plt.figure(figsize=(10, 8))
 
 ax = fig.add_subplot(1, 1, 1)
-ax.scatter(spikeTimes, spikingNeurons, c='blue', marker='.', lw=0)
-ax.set_title('Raster plot')
+ax.scatter(spikeTimes, spikingNeurons, color='blue', marker='.', lw=0)
+for t in samplingTimes:
+    if t*1000 < 1000:
+        ax.plot((t*1000, t*1000), (0, 4000), c='0.25', lw=0.5)
+ax.set_ylim([0, 4000])
+ax.set_ylabel(ylabel='Neuron number', color='blue')
+
+ax2 = ax.twinx()
+ax2.plot(samplingTimes[1:]*1000, route, color='red', marker='o', lw=0)
+ax2.set_ylim([0, 2*np.pi])
+ax2.set_ylabel(ylabel='place', color='red')
+
+ax.set_title('Place decoding from spike train')
 ax.set_xlim([0, 10000])
 ax.set_xlabel('Time [ms]')
-ax.set_ylim([0, 4000])
-ax.set_ylabel('Neuron number')
 
-plt.show()
+fName = os.path.join(SWBasePath, 'figures', 'place_decoding_'+str(tempRes)+'.jpg')
+fig.savefig(fName)
