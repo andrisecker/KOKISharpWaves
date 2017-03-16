8af9f94ec297f2b3fabce3d0927e734b7c592831
András Ecker <andras.ecker@epfl.ch>
2017-03-16 21:00:11 +0100

    STDP plots added

--- scripts/plots.py
+++ scripts/plots.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf8 -*-
 '''
-helper file to plot dynamics
+helper file to plot dynamics (and the weight matrix)
 author: András Ecker, last update: 03.2017
 '''
 
@@ -13,20 +13,20 @@ SWBasePath = '/'.join(os.path.abspath(__file__).split('/')[:-2])
 figFolder = os.path.join(SWBasePath, "figures")
 
 
-def plot_PSD(rate, rippleAC, f, Pxx, title, linespec_, multiplier):
-    '''
+def plot_PSD(rate, rippleAC, f, Pxx, title_, linespec_, multiplier_):
+    """
     saves figure with rate, auto-correlation plot, and PSD
     :param rate: population rate (produced by Brian population rate monitor)
     :param rippleAC: auto-correlation function of the rate (returned by ripple())
     :param f, Pxx (returned by PSD analysis) see more: http://docs.scipy.org/doc/scipy-dev/reference/generated/scipy.signal.welch.html
-    :param title, linespec_, multiplier: outline and naming parameters
-    '''
+    :param title_, linespec_, multiplier: outline and naming parameters
+    """
 
     fig = plt.figure(figsize=(10, 8))
 
     ax = fig.add_subplot(3, 1, 1)
     ax.plot(np.linspace(0, 10000, len(rate)), rate, linespec_)
-    ax.set_title("%s rate"%title)
+    ax.set_title("%s rate"%title_)
     ax.set_xlabel('Time [ms]')
 
 
@@ -68,17 +68,17 @@ def plot_PSD(rate, rippleAC, f, Pxx, title, linespec_, multiplier):
 
     fig.tight_layout()
 
-    figName = os.path.join(figFolder, "%s_%s.png"%(multiplier, title))
+    figName = os.path.join(figFolder, "%s_%s.png"%(multiplier_, title_))
     fig.savefig(figName)
     
     
-def plot_zoomed(rate, spikes, title, color_, linespec_, multiplier):
-    '''
+def plot_zoomed(rate, spikes, title_, color_, linespec_, multiplier_):
+    """
     saves figure with zoomed in raster and rate (last 100ms)
     :param rate: population rate (produced by Brian population rate monitor)
     :param spikes: tuple with neuron IDs and spike times (produced by Brian spike monitor)
-    :param title, color_, linespec_, multiplier: outline and naming parameters
-    '''
+    :param title_, color_, linespec_, multiplier_: outline and naming parameters
+    """
     
     fig = plt.figure(figsize=(10, 8))
 
@@ -102,7 +102,7 @@ def plot_zoomed(rate, spikes, title, color_, linespec_, multiplier):
 
     ax = fig.add_subplot(2, 1, 1)
     ax.scatter(rasterX, rasterY, c=color_, marker='.', lw=0)
-    ax.set_title("%s raster (last 100 ms)"%title)
+    ax.set_title("%s raster (last 100 ms)"%title_)
     ax.set_xlim([9900, 10000])
     ax.set_xlabel("Time [ms]")
     ax.set_ylim([ymin, ymax])
@@ -115,7 +115,113 @@ def plot_zoomed(rate, spikes, title, color_, linespec_, multiplier):
 
     fig.tight_layout()
 
-    figName = os.path.join(figFolder, "%s_%s_zoomed.png"%(multiplier, title))
+    figName = os.path.join(figFolder, "%s_%s_zoomed.png"%(multiplier_, title_))
+    fig.savefig(figName)
+
+
+def plot_wmx(wmx, saveName_):
+    """
+    Plots the weight matrix
+    :param wmx: ndarray representing the weight matrix
+    :param saveName_: name of saved img
+    """
+    
+    fig = plt.figure(figsize=(10, 8))
+    ax = fig.add_subplot(1, 1, 1)
+    i = ax.imshow(wmx, interpolation='None')
+    fig.colorbar(i)
+    ax.set_title("Learned synaptic weights")
+    
+    figName = os.path.join(figFolder, "%s.png"%saveName_)
+    fig.savefig(figName)
+  
+  
+def plot_wmx_avg(wmx, nPop, saveName_):
+    """
+    Plots the averaged weight matrix (better view as a whole)
+    :param wmx: ndarray representing the weight matrix
+    :param nPop: number of populations
+    :param saveName_: name of saved img
+    """ 
+    
+    assert 4000 % nPop == 0
+
+    popSize = int(4000.0 / nPop)
+    wmxM = np.zeros((100, 100))
+    for i in range(nPop):
+        for j in range(nPop):
+            tmp = wmx[int(i*popSize):int((i+1)*popSize), int(j*popSize):int((j+1)*popSize)]
+            wmxM[i, j] = np.mean(tmp)
+
+    fig = plt.figure(figsize=(10, 8))
+    ax = fig.add_subplot(1, 1, 1)
+    i = ax.imshow(wmxM, interpolation='None')
+    fig.colorbar(i)
+    ax.set_title("Learned synaptic weights (avg.)")
+    
+    figName = os.path.join(figFolder, "%s.png"%saveName_)
+    fig.savefig(figName)
+    
+
+def plot_w_distr(wmx, saveName_):
+    """
+    Plots the distribution of the weights
+    :param wmx: ndarray representing the weight matrix
+    :param saveName_: name of saved img
+    """  
+    
+    # deleting nulls from wmx to plot the distribution of the weights
+    tmp = wmx.tolist()
+    wmx = [val for sublist in tmp for val in sublist]
+    wmx = filter(lambda i: i != 0, wmx)
+    wmx = np.array(wmx)
+    log10wmx = np.log10(wmx)
+    # print("mean:", np.mean(wmx))
+
+    fig = plt.figure(figsize=(10, 8))
+
+    ax = fig.add_subplot(2, 1, 1)
+    ax.hist(wmx, bins=150)
+    ax.set_title('Distriboution of synaptic weights')
+    ax.set_xlabel('pyr-pyr synaptic weight strength [nS]')
+    ax.set_ylabel('# of synapses (on logarithmic scale)')
+    plt.yscale('log')
+
+    ax2 = fig.add_subplot(2, 1, 2)
+    ax2.hist(log10wmx, bins=150, color='red')
+    ax2.set_title('Distribution of synaptic weights')
+    ax2.set_xlabel('log10(pyr-pyr synaptic weight strength) [nS]')
+    ax2.set_ylabel('# of synapses (on logarithmic scale)')
+    plt.yscale('log')
+
+    fig.tight_layout()
+    
+    figName = os.path.join(figFolder, "%s.png"%saveName_)
     fig.savefig(figName)
     
     
+def plot_STDP_rule(taup, taum, Ap, Am, saveName_):
+    """
+    Plots the STDP rule used for learning
+    exponential STDP: f(s) = A_p * exp(-s/tau_p) (if s > 0), where s=tpost_{spike}-tpre_{spike}
+    :param taup, taum: time constant of weight change
+    :param Ap, Am: max amplitude of weight change
+    """
+
+    delta_t = np.linspace(-100, 100, 1000)
+    delta_w = np.where(delta_t<0, Ap*np.exp(delta_t/taup), Am*np.exp(-delta_t/taum))
+    
+    fig = plt.figure(figsize=(10, 8))
+
+    ax = fig.add_subplot(1, 1, 1)
+    ax.plot(delta_t, delta_w)
+    ax.set_xlabel("delta_t [ms]")
+    ax.set_ylabel("delta_w")
+    ax.set_ylim(-Am*1.05, Ap*1.05)
+    ax.set_xlim([-70, 70])
+    ax.axhline(0, ls='-', c='k')
+    
+    figName = os.path.join(figFolder, "%s.png"%saveName_)
+    fig.savefig(figName)
+    plt.close()
+     
