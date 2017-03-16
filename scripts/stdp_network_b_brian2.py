b7e6ebe02c86ac05cd2d5803f82671fca01387da
András Ecker <andras.ecker@epfl.ch>
2017-03-16 21:01:03 +0100

    running version!

--- scripts/stdp_network_b_brian2.py
+++ scripts/stdp_network_b_brian2.py
@@ -9,9 +9,12 @@ author: András Ecker last update: 03.2017
 '''
 
 from brian2 import *
+set_device('cpp_standalone')  # speed up the simulation with generated C++ code
+from brian2tools import *  # just for spec. plotting
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+from plots import plot_wmx, plot_wmx_avg, plot_w_distr, plot_STDP_rule
 
 fIn = 'spikeTrainsR.npz'
 fOut = 'wmxR_sym.txt'
@@ -27,139 +30,102 @@ spikeTrains = npzFile['spikeTrains']
 
 # create 2 numpy arrays for Brian2's SpikeGeneratorGroup
 spikingNrns = 0 * np.ones(len(spikeTrains[0]))
-spikeTimes = np.asarray(spikeTrains[0])*1000  # *1000 for ms convertion
+spikeTimes = np.asarray(spikeTrains[0])
 for neuron in range(1, N):
     nrn = neuron * np.ones(len(spikeTrains[neuron]))
     spikingNrns = np.concatenate((spikingNrns, nrn), axis=0)
-    tmp = np.asarray(spikeTrains[neuron])*1000  # *1000 for ms convertion
+    tmp = np.asarray(spikeTrains[neuron])
     spikeTimes = np.concatenate((spikeTimes, tmp), axis=0)
 
 print "spike times loaded"
 
 # with default dt=100*us there are neurons whose emit more then 1 spike per timestep
-PC = SpikeGeneratorGroup(N, spikingNrns, spikeTimes*ms, dt=10*us)  
+PC = SpikeGeneratorGroup(N, spikingNrns, spikeTimes*second, dt=10*us)  
 
-def learning(spikingNeuronGroup):
-    '''
+# STDP parameters
+taup = taum = 20  # ms
+Ap = Am = 0.01  # : 1
+wmax = 7.5  # nS
+Ap *= wmax
+Am *= wmax
+
+
+def learning(spikingNeuronGroup, taup, taum, Ap, Am, wmax):
+    """
     Takes a spiking group of neurons, connects the neurons sparsely with each other,
-    and learns the weight 'pattern' via STDP
+    and learns the weight 'pattern' via STDP:
+    exponential STDP: f(s) = A_p * exp(-s/tau_p) (if s > 0), where s=tpost_{spike}-tpre_{spike}
     :param spikingNeuronGroup: Brian class of spiking neurons
+    :param taup, taum: time constant of weight change
+    :param Ap, Am: max amplitude of weight change
+    :param wmax: maximum weight
     :return weightmx: numpy ndarray with the learned synaptic weights
-            SpikeM: SpikeMonitor of the network (for plotting and further analysis)
-            STDP: Brian Synapse class (for plotting and further analysis)
-    '''
+            spikeM: SpikeMonitor of the network (for plotting and further analysis)
+    """
 
-    # exponential STDP: f(s) = A_p * exp(-s/tau_p) (if s > 0), where A_p = 0.01, tau_p = 20*ms and s=tpost_{spike}-tpre_{spike}
-    # see more: http://brian2.readthedocs.io/en/stable/resources/tutorials/2-intro-to-brian-synapses.html   
-    taup = taum = 20*ms
-    Ap = Am = 0.01
-    wmax = 7.5  # nS
-    Ap *= wmax
-    Am *= wmax
+    plot_STDP_rule(taup, taum, Ap, Am, "STDP_rule_Brian2")
+    
+    # plot above needs dimensionless arguments
+    taup *= ms
+    taum *= ms
+
     # mimics Brian1's exponentialSTPD class, with interactions='all', update='additive'
     # see more on conversion: http://brian2.readthedocs.io/en/stable/introduction/brian1_to_2/synapses.html
     STDP = Synapses(spikingNeuronGroup, spikingNeuronGroup,
-             '''
+             """
              w : 1
              dA_pre/dt = -Ap/taup : 1 (event-driven)
              dA_post/dt = -Am/taum : 1 (event-driven)
-             ''',
-             on_pre='''
+             """,
+             on_pre="""
              A_pre += Ap
-             w = w + A_post
              w = clip(w + A_post, 0, wmax)
-             ''',
-             on_post='''
+             """,
+             on_post="""
              A_post += Am
-             w = w + A_pre
              w = clip(w + A_pre, 0, wmax)
-             ''',
-             dt=10*us)  # small dt is only to match the dt of SpikeGeneratorGroup
+             """)#,
+             #dt=10*us)  # small dt is only to match the dt of SpikeGeneratorGroup
              
     STDP.connect(condition="i!=j", p=0.16)
     STDP.w = 0.1 # nS
 
+
     # run simulation
-    SpikeM = SpikeMonitor(spikingNeuronGroup, record=True)
-    #SM = StateMonitor(STDP, 'w', record=True)  # TODO: this throws segmentation error...
-    run(100*second, report='text')  # the generated spike train is 500 sec long...
+    spikeM = SpikeMonitor(spikingNeuronGroup, record=True)
+    run(400*second, report='text')  # the generated spike train is 500 sec long...
     
     # weight matrix
-    weightmx = STDP.w[:, :, :]
-    #weightmx = np.reshape(weightmx, (4000, 4000))
-    # np.fill_diagonal(weightmx, 0)  # avoided by condition...
+    weightmx = np.zeros((4000, 4000))
+    weightmx[STDP.i[:], STDP.j[:]] = STDP.w[:]
 
-    return weightmx, SpikeM, STDP
-
-
-weightmx, SpikeM, STDP = learning(PC)
+    return weightmx, spikeM, STDP
 
 
-fig0 = plt.figure(figsize=(10,8))
+weightmx, spikeM, STDP = learning(PC, taup, taum, Ap, Am, wmax)
 
-ax = fig0.add_subplot(1, 1, 1)
-ax.plot(SP.t/ms, SP.i, '.b')
-ax.set_title("raster plot")
-
-plt.show()
-
-"""
+# Plots: raster
 fig = plt.figure(figsize=(10, 8))
 ax = fig.add_subplot(1, 1, 1)
-i = ax.imshow(weightmx, interpolation='None')
-fig.colorbar(i)
+brian_plot(spikeM, axes=ax)
+ax.set_title("Raster plot")
+
+#plt.show()
+
+plot_wmx(weightmx, "wmx_Brian2")
+plot_wmx_avg(weightmx, 100, "wmx_avg_Brian2")
+plot_w_distr(weightmx, "w_distr_Brian2")
+
+
+"""
+# check if Brian2 figure is the same:
+fig = plt.figure(figsize=(10, 8))
+ax = plot_synapses(STDP.i, STDP.j, STDP.w, var_name='synaptic weights', plot_type='scatter', cmap='hot')
+add_background_pattern(ax)
 ax.set_title('Learned weight matrix')
+"""
 
-
-# averaged figure! (better view as a whole...)
-nPop = 100
-popSize = 4000.0 / nPop
-wmxM = np.zeros((100, 100))
-for i in range(nPop):
-    for j in range(nPop):
-        tmp = weightmx[i*popSize:(i+1)*popSize, j*popSize:(j+1)*popSize]
-        wmxM[i, j] = np.mean(tmp)
-
-
-fig2 = plt.figure(figsize=(10, 8))
-ax = fig2.add_subplot(1, 1, 1)
-i = ax.imshow(wmxM, interpolation='None')
-fig2.colorbar(i)
-ax.set_title('Learned weight matrix (averaged)')
-
-
-# deleting nulls from wmx to plot the distribution of the weights
-wmx = weightmx.tolist()
-wmx = [val for sublist in wmx for val in sublist]
-wmx = filter(lambda i: i != 0, wmx)
-wmx = np.array(wmx)
-log10wmx = np.log10(wmx)
-print("mean:", np.mean(wmx))
-
-
-fig3 = plt.figure(figsize=(10, 8))
-
-ax = fig3.add_subplot(2, 1, 1)
-ax.hist(wmx, bins=150)
-ax.set_title('Distriboution of synaptic weights')
-ax.set_xlabel('pyr-pyr synaptic weight strength [nS]')
-#ax.set_xlim([0, 28])
-ax.set_ylabel('# of synapses (on logarithmic scale)')
-plt.yscale('log')
-
-ax2 = fig3.add_subplot(2, 1, 2)
-ax2.hist(log10wmx, bins=150, color='red')
-ax2.set_title('Distribution of synaptic weights')
-ax2.set_xlabel('log10(pyr-pyr synaptic weight strength) [nS]')
-ax2.set_ylabel('# of synapses (on logarithmic scale)')
-#ax2.set_xlim(-200, 2)
-plt.yscale('log')
-
-fig3.tight_layout()
-
-plt.show()
 
 # save weightmatrix
 fName = os.path.join(SWBasePath, 'files', fOut)
-np.savetxt(fName, weightmx)
-"""
+#np.savetxt(fName, weightmx)
