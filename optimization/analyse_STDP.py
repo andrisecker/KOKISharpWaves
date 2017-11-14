89de581a136872cb55cea884f3c2c9c4b8c4d809
András Ecker <andras.ecker@epfl.ch>
2017-11-14 22:11:50 +0100

    STDP analysis finished

--- optimization/analyse_STDP.py
+++ optimization/analyse_STDP.py
@@ -7,18 +7,19 @@ author: András Ecker last update: 11.2017
 """
 
 import os
-#import sys
+import json
 from brian2 import *
 import numpy as np
 import random as pyrandom
-#import matplotlib.pyplot as plt
-from analyse_EPS import sym_paired_recording
+from scipy.optimize import curve_fit
+import matplotlib.pyplot as plt
+from analyse_EPS import sym_paired_recording, get_peak_EPSP
 SWBasePath = os.path.sep.join(os.path.abspath('__file__').split(os.path.sep)[:-2])
 sys.path.insert(0, os.path.sep.join([SWBasePath, 'scripts']))
-from plots import plot_STDP_rule
+from plots import plot_STDP_rule, plot_learned_EPSPs, plot_compare_STDP_to_orig, plot_STDP2
 
 
-def sim_pairing_prot(delta_ts, taup, taum, Ap, Am, wmax, w_init):  # STDP_synapse
+def sim_pairing_prot(delta_ts, taup, taum, Ap, Am, wmax, w_init):
     """
     Aims to mimic spike pairing induced LTP protocol from Mishra et al. 2016 (300 repetition with different $delta$t-s at 1Hz)
     (Simulated for all different $delta$t-s in the same time, since it's way more effective than one-by-one)
@@ -72,33 +73,118 @@ def sim_pairing_prot(delta_ts, taup, taum, Ap, Am, wmax, w_init):  # STDP_synaps
     return STDP.w[:]
 
 
+def get_EPSP_change(delta_ts, dPeakEPSPs):
+    """calculate the change (in percantage as in Mishra et al. 2016) of PSP amplitude"""
+    
+    dEPSPchanges = {"time":[], "change":[]}
+    baselineEPSP = dPeakEPSPs["baseline"]
+    for delta_t in delta_ts:
+        peakEPSP = dPeakEPSPs[delta_t]
+        dEPSPchanges["time"].append(delta_t)
+        dEPSPchanges["change"].append(((peakEPSP*100.) / baselineEPSP) - 100.)  # percentage change from 100% 
+        
+    return dEPSPchanges
+
+
+def _f_exponential(x, A, tau):
+    """helper function to `scipy.optimize.curve_fit`"""
+    
+    return A * np.exp(-x/tau)
+  
+
+def fit_exponential(delta_ts, dEPSPchanges, A_0, tau_0):
+    """
+    fit exponential curve to simulated EPSP changes (to compare with the STDP rule later)
+    same form as STDP rule: f(s) = A_p * exp(-s/tau_p) (if s > 0), where s=tpost_{spike}-tpre_{spike}
+    """
+    
+    # preprocess data
+    if len(dEPSPchanges) == 2:  # simulated values
+        xdata = []; ydata = [];
+        delta_ts = dEPSPchanges["time"]; changes = dEPSPchanges["change"]
+        for delta_t, change in zip(delta_ts, changes):
+            if delta_t > 0:
+                xdata.append(delta_t); ydata.append(change)
+    
+    else:  # the dict is the dOrigData () -> in vitro values
+        xdata = []; ydatap = []; ydatam = [];
+        delta_ts = dEPSPchanges["time(ms)"]; changes = dEPSPchanges["mean(%)"]
+        for delta_t, change in zip(delta_ts, changes):
+            if delta_t > 0:
+                xdata.append(delta_t); ydatap.append(change)
+            elif delta_t < 0:  # reuse negative valeus (the fitted exponential will be symmetrical any way)
+                ydatam.append(change)            
+        ydata = [(a+b)/2. for a, b in zip(ydatap, ydatam[::-1])]  # average pos. and neg. values
+    
+    # optimize parameters
+    popt, pcov = curve_fit(_f_exponential, xdata, ydata, p0=(A_0, tau_0), bounds=(0, [np.inf, 100]), method="trf")
+    
+    return popt
+
+
+
 if __name__ == "__main__":
     
-    delta_ts = [-100., -50., -10., 10., 50., 100.]  # ms
+    #delta_ts = [-100., -50., -20., -10., 10., 20., 50., 100.]  # (ms) same as Mishra et al. 2016
+    delta_ts = [-50., 25., 35., 45., 55., 65., 75., 100.]  # (ms) more diverse values to fit better exponential
     
     # STDP parameters
     taup = taum = 20 * ms
-    Ap = Am = 0.01  # 0.0035
+    Ap = Am = 0.01
     wmax = 8e-9  # S (w is dimensionless in the equations)
     Ap *= wmax  # needed to reproduce Brian1 results
     Am *= wmax  # needed to reproduce Brian1 results
-    w_init = 1e-9  # S (w is dimensionless in the equations)
     
     mode_ = plot_STDP_rule(taup/ms, taum/ms, Ap/1e-9, Am/1e-9, "STDP_rule")
     
+    # w:0.54 as mean value from Guzman 2016 (from Kali - Guzmann e-mail: 0.52...) -> but that's after learning...
+    w_init = 0.1e-9  # S (w is dimensionless in the equations)
+        
+    # baseline EPS
+    v_hold = -70  # mV
     i_hold = -43.638  # pA (calculated by clamp_cell.py)
-    
-    # baseline EPSP
-    t_, EPSP, _ = sym_paired_recording(w_init, i_hold)
-    dEPSPs = {"t":t_, "baseline":EPSP}
+    t_, EPSP, _ = sym_paired_recording(w_init*1e9, i_hold)  # * 1e9 nS conversion
+    peakEPSP = get_peak_EPSP(t_, EPSP, i_hold, v_hold)
+    dEPSPs = {"t":t_, "baseline":EPSP}; dPeakEPSPs = {"baseline":peakEPSP}
     
     # apply spike pairing LTP protocol     
     weights = sim_pairing_prot(delta_ts, taup, taum, Ap, Am, wmax, w_init)
     
     # EPSPs after learning
+    max_peakEPSP = 0  # later used as initial guess of A, during fitting the exponential
     for delta_t, weight in zip(delta_ts, weights):
-        _, EPSP, _ = sym_paired_recording(weight, i_hold)
-        dEPSPs[delta_t] = EPSP
+        t_, EPSP, _ = sym_paired_recording(weight*1e9, i_hold)  # get trace  * 1e9 nS conversion
+        peakEPSP = get_peak_EPSP(t_, EPSP, i_hold, v_hold)  # extract peak EPSP
+        dEPSPs[delta_t] = EPSP; dPeakEPSPs[delta_t] = peakEPSP  # store results
+        max_peakEPSP = peakEPSP if peakEPSP > max_peakEPSP else max_peakEPSP
+        
+    # plot EPSPs after learning
+    plot_learned_EPSPs(delta_ts, dEPSPs, "learned_EPSPs")
+        
+    dEPSPchanges = get_EPSP_change(delta_ts, dPeakEPSPs)  # get the EPSP changes in percentage
+    
+    
+    # load in in vitro data (kindly provided by Jose Guzman and converted to a simplified .json by us)
+    fName = os.path.join(SWBasePath, "files", "original_STDP_data.json")
+    with open(fName, "r") as f_:
+        dOrigData = json.load(f_)
+    
+    # fit exponential in additon to the Gaussian fit
+    A, tau = fit_exponential(delta_ts, dOrigData, A_0=1.5, tau_0=70)  # A_0 and tau_0 is setted by hand...
+    dOrigEFit = {"Ap": A, "Am": A, "taup": tau, "taum": tau}
+    
+    # fit exp function to the changes of simulated EPSPs
+    A, tau = fit_exponential(delta_ts, dEPSPchanges, A_0=max_peakEPSP, tau_0=taup/ms)
+    dFittedChanges = {"Ap": A, "Am": A, "taup": tau, "taum": tau}
+        
+    # plot them together!
+    plot_compare_STDP_to_orig(dEPSPchanges, dOrigData, "compare_learning", dOrigEFit=dOrigEFit, dFittedChanges=dFittedChanges)    
+    # plot STDP rule and fitted changes together
+    dSTDPrule = {"Ap": Ap/1e-9, "Am": Am/1e-9, "taup": taup/ms, "taum": taum/ms}    
+    plot_STDP2(dSTDPrule, dFittedChanges, mode_, "compare_STDP")
+    
+    plt.show()
+    
 
 
     
--- scripts/plots.py
+++ scripts/plots.py
@@ -7,7 +7,11 @@ authors: Bence Bagi, András Ecker, last update: 09.2017
 
 import os
 import numpy as np
+import seaborn as sns
 import matplotlib.pyplot as plt
+
+#sns.set_context("paper")
+sns.set_style("white")
 
 SWBasePath = os.path.sep.join(os.path.abspath(__file__).split(os.path.sep)[:-2])
 figFolder = os.path.join(SWBasePath, "figures")
@@ -24,7 +28,7 @@ def plot_raster_ISI(spikeTimes, spikingNeurons, hist, color_, multiplier_):
 
     fig = plt.figure(figsize=(10, 8))
     ax = fig.add_subplot(2, 1, 1)
-    ax.scatter(spikeTimes, spikingNeurons, c=color_, marker='.', lw=0)
+    ax.scatter(spikeTimes, spikingNeurons, c=color_, marker='.', linewidth=0)
     ax.set_title("Pyr_population raster")
     ax.set_xlim([0, 10000])
     ax.set_xlabel("Time (ms)")
@@ -32,7 +36,7 @@ def plot_raster_ISI(spikeTimes, spikingNeurons, hist, color_, multiplier_):
     ax.set_ylabel("Neuron number")
 
     ax2 = fig.add_subplot(2, 1, 2)
-    ax2.bar(hist[1][:-1], hist[0], width=50, align="edge", color=color_, edgecolor='black', lw=0.5, alpha=0.9)
+    ax2.bar(hist[1][:-1], hist[0], width=50, align="edge", color=color_, edgecolor='black', linewidth=0.5, alpha=0.9)
     ax2.axvline(150, ls='--', c="gray", label="ROI for replay analysis")
     ax2.axvline(850, ls='--', c="gray")
     ax2.set_title("Pyr_population ISI distribution")
@@ -138,7 +142,7 @@ def plot_zoomed(spikeTimes, spikingNeurons, rate, title_, color_, multiplier_, P
     fig = plt.figure(figsize=(10, 8))
 
     ax = fig.add_subplot(2, 1, 1)
-    ax.scatter(rasterX, rasterY, c=color_, marker='.', lw=0)
+    ax.scatter(rasterX, rasterY, c=color_, marker='.', linewidth=0)
     ax.set_title("%s raster (last 100 ms)"%title_)
     ax.set_xlim([9900, 10000])
     ax.set_xlabel("Time (ms)")
@@ -345,10 +349,10 @@ def plot_STDP_rule(taup, taum, Ap, Am, saveName_):
     fig = plt.figure(figsize=(10, 8))
 
     ax = fig.add_subplot(1, 1, 1)
-    ax.plot(delta_t, delta_w, label="STDP rule taup:%s(ms), Ap:%s"%(taup, Ap))
+    ax.plot(delta_t, delta_w, "b-", linewidth=2, label="STDP rule taup:%s(ms), Ap:%s"%(taup, Ap))
     ax.set_title("STDP curve")
-    ax.set_xlabel("delta_t /post-pre/ (ms)")
-    ax.set_ylabel("delta_w (nS)")
+    ax.set_xlabel("$\Delta t$ /post-pre/ (ms)")
+    ax.set_ylabel("$\Delta w$ (nS)")
     if mode == "asym":
         ax.set_ylim([-Ap*1.05, Ap*1.05])
     elif mode == "sym":
@@ -359,7 +363,6 @@ def plot_STDP_rule(taup, taum, Ap, Am, saveName_):
 
     figName = os.path.join(figFolder, "%s_%s.png"%(saveName_, mode))
     fig.savefig(figName)
-    plt.close()
 
     return mode
 
@@ -736,6 +739,125 @@ def plot_EPS_dist(peakEPSPs, peakEPSCs, saveName_):
     
     fig.tight_layout()
     figName = os.path.join(figFolder, "%s.png"%saveName_)
-    fig.savefig(figName)  
+    fig.savefig(figName)
+
+
+def plot_learned_EPSPs(delta_ts, dEPSPs, saveName_):
+    """
+    saves plot with EPSPs after learning (via STDP)
+    :param delta_ts: list of $delta$t intervals between pre and post spikes (in ms)
+    :param dEPSPs: dictonary with keys of delta_ts and EPSP traces as values
+    :param saveName_: name of saved img
+    """
+    
+    assert len(delta_ts) == 8, "plot works only for 2*4 delta_ts..."
+    t = dEPSPs["t"]
+    
+    fig = plt.figure(figsize=(20, 8))
+    
+    for i, delta_t in enumerate(delta_ts):
+        ax = fig.add_subplot(2, 4, i+1)
+        ax.plot(t, dEPSPs["baseline"], "k--", linewidth=1.5)
+        col = "b" if delta_t > 0 else "r"
+        ax.plot(t, dEPSPs[delta_t], color=col, linewidth=2)
+        ax.set_title("$\Delta t$ /post-pre/ = %s (ms)"%delta_t)
+        ax.set_xlim([0, 400])
+        ax.set_ylabel("EPSP (mV)")
+        if i >= 4:
+            ax.set_xlabel("Time (ms)")
+        
+    fig.tight_layout()
+    figName = os.path.join(figFolder, "%s.png"%saveName_)
+    fig.savefig(figName)
+       
+    
+def plot_compare_STDP_to_orig(dEPSPchanges, dOrigData, saveName_, dOrigEFit=None, dFittedChanges=None):
+    """
+    saves plot based on Figure 1 i) in Mishra et al. 2016 - 10.1038/ncomms11552, extended with the in silico EPSP changes (with fixed cell model and STDP rule)
+    :param dEPSPchanges: dictonary with keys of delta_ts and values of EPSP changes in percentage
+    :param dOrigData: dictionary of original values (might not be straightforwad to understand)
+    :param saveName_: name of saved img
+    :param dOrigEFit: optional dictionary with taup, taum: time constant of weight change & Ap, Am: max amplitude of weight change (fitted to original data)
+    :param dFittedChanges: same dictionary as above (fitted to simulated data)
+    """
+    
+    fig = plt.figure(figsize=(10, 8))    
+    ax = fig.add_subplot(1, 1, 1)
+    
+    # plot original data
+    ax.plot(dOrigData["time(ms)"], dOrigData["mean(%)"], "ko", markersize=6, label="original (in vitro) data")
+    ax.errorbar(dOrigData["time(ms)"], dOrigData["mean(%)"], yerr=dOrigData["sem"], fmt="none", ecolor="k")
+    # plot fitted Gaussian (to original data)
+    t_ = np.arange(-150, 150, 0.1)
+    fit_G = dOrigData["gauss_a"] * np.exp(-((t_ - dOrigData["gauss_b"])**2) / (2*dOrigData["gauss_c"]**2))
+    ax.plot(t_, fit_G, "r-", linewidth=2, label="Gaussian fit (orig. data)")
+    if dOrigEFit:  # plot exponential fit (to original data)
+        fit_E = np.where(t_>0, dOrigEFit["Ap"]*np.exp(-t_/dOrigEFit["taup"]), dOrigEFit["Am"]*np.exp(t_/dOrigEFit["taum"]))
+        ax.plot(t_, fit_E, color="orange", linewidth=2, label="exponential fit (orig. data) taup:%.3f(ms)"%dOrigEFit["taup"])
+    # plot in silico data
+    ax2 = ax.twinx()
+    ax2.plot(dEPSPchanges["time"], dEPSPchanges["change"], "go", markersize=6, label="simulated data")    
+    if dFittedChanges:  # plot exponential fit (to simulated data)
+        fit_E = np.where(t_>0, dFittedChanges["Ap"]*np.exp(-t_/dFittedChanges["taup"]), dFittedChanges["Am"]*np.exp(t_/dFittedChanges["taum"]))
+        ax2.plot(t_, fit_E, "g-", linewidth=2, label="exponential fit (sim data) taup:%.3f(ms)"%dFittedChanges["taup"])
+    
+    ax.set_title("Compare in vitro and in silico EPSP changes")
+    ax.set_xlabel("$\Delta t$ /post-pre/ (ms)")
+    ax.set_xlim([-150, 150])
+    ax.set_ylabel("Change in EPSP amplitude (%)", color="black")
+    ax2.set_ylabel("Change in EPSP amplitude (%)", color="green")
+
+    h1, l1 = ax.get_legend_handles_labels()
+    h2, l2 = ax2.get_legend_handles_labels()
+    ax.legend(h1+h2, l1+l2)
+    
+    figName = os.path.join(figFolder, "%s.png"%saveName_)
+    fig.savefig(figName)
+
+
+def plot_STDP2(dSTDPrule, dFittedChanges, mode, saveName_):
+    """
+    Plots the STDP rule used for learning
+    exponential STDP: f(s) = A_p * exp(-s/tau_p) (if s > 0), where s=tpost_{spike}-tpre_{spike}
+    :param dSTDPrule: dictionary with taup, taum: time constant of weight change & Ap, Am: max amplitude of weight change (used to specify STDP rule for learning)
+    :param dFittedChanges: same dictionary as above (fitted to EPSP % changes)
+    :param mode: returned by `plot_STDP_rule` (used only for ylim and naming)
+    :return mode: just for saving conventions
+    """
+
+    delta_t = np.linspace(-150, 150, 1000)
+    delta_w_rule = np.where(delta_t<0, dSTDPrule["Am"]*np.exp(delta_t/dSTDPrule["taum"]), None)
+    delta_w_fitted = np.where(delta_t>0, dFittedChanges["Ap"]*np.exp(-delta_t/dFittedChanges["taup"]), None)
+
+    fig = plt.figure(figsize=(10, 8))
+
+    ax = fig.add_subplot(1, 1, 1)
+    ax.plot(delta_t, delta_w_rule, "b-", linewidth=2, label="STDP rule taup:%s(ms), Ap:%s"%(dSTDPrule["taup"], dSTDPrule["Ap"]))
+    ax.axhline(0, ls='-', c='k')  
+    ax.set_title("STDP")
+    ax.set_xlabel("$\Delta t$ /post-pre/ (ms)")
+    ax.set_xlim([-150, 150])
+    ax.set_ylabel("$\Delta w$ (nS)", color="blue")
+    
+    ax2 = ax.twinx()
+    ax2.plot(delta_t, delta_w_fitted, "g-", linewidth=2, label="fitted 'STDP' taup:%.3f(ms), Ap:%.3f"%(dFittedChanges["taup"], dFittedChanges["Ap"]))
+    ax2.set_ylabel("Change in EPSP amplitude (%)", color="green")
+    
+    if mode == "asym":
+        ax.set_ylim([-1*dSTDPrule["Ap"]*1.05, dSTDPrule["Ap"]*1.05])
+        ax2.set_ylim([-1*dFittedChanges["Ap"]*1.05, dFittedChanges["Ap"]*1.05])
+    elif mode == "sym":
+        ax.set_ylim([-1*dSTDPrule["Ap"]*0.05, dSTDPrule["Ap"]*1.05])
+        ax2.set_ylim([-1*dFittedChanges["Ap"]*0.05, dFittedChanges["Ap"]*1.05])
+  
+    h1, l1 = ax.get_legend_handles_labels()
+    h2, l2 = ax2.get_legend_handles_labels()
+    ax.legend(h1+h2, l1+l2)
+
+    figName = os.path.join(figFolder, "%s_%s.png"%(saveName_, mode))
+    fig.savefig(figName)
+    
+
+
 
 
